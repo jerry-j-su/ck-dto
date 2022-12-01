a311ccc7bd50f63039a55ba8d4deec9b22953112
jerry.j.su <jerrysu.jie@hotmail.com>
2022-12-01 23:32:23 +0800

    [temp]fix nested flexbox overflow issue

--- src/components/OrderList.tsx
+++ src/components/OrderList.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react'
 import useOrders from '../state/useOrders'
 import './OrderList.scss'
 
-export default function OrderList () {
+export default function OrderList() {
     const { orderList, filteredOrderList, orderCount } = useOrders()
 
     const [ordersToShow, setOrderToShow] = useState(orderList)
@@ -13,38 +13,40 @@ export default function OrderList () {
     }, [filteredOrderList, orderList, setOrderToShow])
 
     return (
-        <section className="order-list-container">
+        <>
             <span className="order-count">
                 {filteredOrderList
                     ? `Showing ${filteredOrderList && filteredOrderList.length} of ${orderCount} orders`
                     : `Total ${orderCount} orders`
                 }
             </span>
-            <ul className="order-list-body">
-                {ordersToShow && ordersToShow.map(({ id, customer, destination, 'event_name': status, item, price, 'sent_at_second': time }) => (
-                    <li className="order-wrapper" key={`${id}-${status}`}>
-                        <dl>
-                            <div className="top-row">
-                                <dt className="label name">name</dt>
-                                <dd className="value name">{customer}</dd>
-                            </div>
-                            <div className="name-spacer" />
-                            <div className="col-1">
-                                <dt className="label status" style={{display: 'none'}}>Status</dt>
-                                <dd className="value status">{status}</dd>
-                            </div>
-                            <div className="status-spacer" />
-                            <div className="col-2">
-                                <dt className="label items">items</dt>
-                                <dd className="value items">{item}</dd>
-                                <div className="spacer"/>
-                                <dt className="label price">price</dt>
-                                <dd className="value price">{price}</dd>
-                            </div>
-                        </dl>
-                    </li>
-                ))}
-            </ul>
-        </section>
+            <section className="order-list-container">
+                <ul className="order-list-body">
+                    {ordersToShow && ordersToShow.map(({ id, customer, destination, 'event_name': status, item, price, 'sent_at_second': time }) => (
+                        <li className="order-wrapper" key={`${id}-${status}`}>
+                            <dl>
+                                <div className="top-row">
+                                    <dt className="label name">name</dt>
+                                    <dd className="value name">{customer}</dd>
+                                </div>
+                                <div className="name-spacer" />
+                                <div className="col-1">
+                                    <dt className="label status" style={{ display: 'none' }}>Status</dt>
+                                    <dd className="value status">{status}</dd>
+                                </div>
+                                <div className="status-spacer" />
+                                <div className="col-2">
+                                    <dt className="label items">items</dt>
+                                    <dd className="value items">{item}</dd>
+                                    <div className="spacer" />
+                                    <dt className="label price">price</dt>
+                                    <dd className="value price">{price}</dd>
+                                </div>
+                            </dl>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </>
     )
 }
